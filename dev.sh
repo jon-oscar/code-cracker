faa138b7d567b69b48267848a437e2d850a48628
Jon Portella <jportella93@gmail.com>
2024-03-03 09:46:12 -0500

    Add multi tab dev script

---
+++ dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SESS='code-cracker-dev'
+CMD1='cd server && npm run dev'
+CMD2='cd client && npm run dev'
+
+# Create a new session named "$SESS" and run the first command
+tmux new-session -d -s $SESS "$CMD1"
+
+# Split the window vertically and run the second command
+tmux split-window -v -t $SESS "$CMD2"
+
+# Attach to the session
+tmux attach -t $SESS
