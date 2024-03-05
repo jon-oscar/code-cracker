47116b091320e86430c335fbca9a2a1a95522a0e
Jon Portella <jportella93@gmail.com>
2024-03-05 18:58:08 -0500

    Add tailwindcss

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -3,22 +3,31 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout(): JSX.Element {
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       {/* A "layout route" is a good place to put markup you want to
           share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
+
+      <nav className='bg-purple-500 text-white p-4'>
+        <ul className='flex space-x-4'>
           <li>
-            <a href='/'>Home</a>
+            <a href='/' className='hover:underline'>
+              Home
+            </a>
           </li>
           <li>
-            <a href='/about'>About</a>
+            <a href='/about' className='hover:underline'>
+              About
+            </a>
           </li>
           <li>
-            <a href='/contact'>Contact</a>
+            <a href='/contact' className='hover:underline'>
+              Contact
+            </a>
           </li>
           <li>
-            <a href='/user'>User</a>
+            <a href='/user' className='hover:underline'>
+              User
+            </a>
           </li>
         </ul>
       </nav>
@@ -28,6 +37,10 @@ export default function Layout(): JSX.Element {
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
       <Outlet />
+
+      <div className='flex-grow'>
+        <Outlet />
+      </div>
     </div>
   );
 }
