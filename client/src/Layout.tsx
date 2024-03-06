5a8e2ccb9f987a285ceeb4726b6c2d5d849170f5
Jon Portella <jportella93@gmail.com>
2024-03-05 19:08:01 -0500

    Add daisyui

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -3,44 +3,95 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout(): JSX.Element {
   return (
-    <div className='flex flex-col min-h-screen'>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
-
-      <nav className='bg-purple-500 text-white p-4'>
-        <ul className='flex space-x-4'>
-          <li>
-            <a href='/' className='hover:underline'>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href='/about' className='hover:underline'>
-              About
-            </a>
-          </li>
-          <li>
-            <a href='/contact' className='hover:underline'>
-              Contact
-            </a>
-          </li>
-          <li>
-            <a href='/user' className='hover:underline'>
-              User
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <div className='navbar bg-base-100'>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <div
+              tabIndex={0}
+              role='button'
+              className='btn btn-ghost btn-circle'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 6h16M4 12h16M4 18h7'
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+            >
+              <li>
+                <a href='/about'>About</a>
+              </li>
+              <li>
+                <a href='/contact'>Contact</a>
+              </li>
+              <li>
+                <a href='/user'>User</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='navbar-center'>
+          <a href='/' className='btn btn-ghost text-xl'>
+            Code Cracker
+          </a>
+        </div>
+        <div className='navbar-end'>
+          <button className='btn btn-ghost btn-circle'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+              />
+            </svg>
+          </button>
+          <button className='btn btn-ghost btn-circle'>
+            <div className='indicator'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-5 w-5'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
+                />
+              </svg>
+              <span className='badge badge-xs badge-primary indicator-item'></span>
+            </div>
+          </button>
+        </div>
+      </div>
 
       <hr />
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
       <Outlet />
-
-      <div className='flex-grow'>
-        <Outlet />
-      </div>
-    </div>
+    </>
   );
 }
